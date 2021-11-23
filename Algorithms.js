3d0769ebf4c3d52d3fde565f1f262a3a1106abbe
Thuan <ttctde@gmail.com>
2021-11-23 20:48:53 +0700

    init commit

--- Algorithms.js
+++ Algorithms.js
@@ -42,6 +42,7 @@ function findLongestSubstring(str) {
 console.log(findLongestSubstring("aaaaaa"));
 */
 
+
 /////////Recursion ////////
 /*
 function factorial(num) {
@@ -113,5 +114,26 @@ function flatten(arr) {
 }
 
 
+function capitalizeFirst(arr) {
+const result=[];
+for(let value of arr ){
+  result.push((value[0].toUpperCase()+value.slice(1)))
+}
+return result;
+}
+function capitalizeFirst (array) {
+  if (array.length === 1) {
+    return [array[0][0].toUpperCase() + array[0].substr(1)];
+  }
+  const res = capitalizeFirst(array.slice(0, -1));
+  const string = array.slice(array.length - 1)[0][0].toUpperCase() + array.slice(array.length-1)[0].substr(1);
+  console.log(res);
+  res.push(string);
+  return res;
+}
+console.log(capitalizeFirst(['car','taco','banana']));
 
 */
+
+
+
