3738d75bf5a7b0c4b3634227770ffa3c96198c60
Thuan <ttctde@gmail.com>
2021-11-25 18:27:28 +0700

    init commit

--- Algorithms.js
+++ Algorithms.js
@@ -310,8 +310,41 @@ function insertionSort(arr){
 console.log(insertionSort([21,23,41,53,123,41,2,12,42,54]));
 
 
+function merge(arr1, arr2) {
+  let result = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] > arr2[j]) {
+      result.push(arr2[j]);
+      j++;
+    } else {
+      result.push(arr1[i]);
+      i++;
+    }
+  }
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+
+  return result;
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+console.log(mergeSort([21, 23, 41, 53, 123, 41, 2, 12, 42]));
+
 
 */
-
-
-
