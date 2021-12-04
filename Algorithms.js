75d9aace344ebf6cab5a2d22830a56c05a186c4f
Thuan <ttctde@gmail.com>
2021-12-04 23:23:41 +0700

    update Binary Search Tree and Breadth First Search and DFS(3 Pre,In and Post)

--- Algorithms.js
+++ Algorithms.js
@@ -836,6 +836,246 @@ class Queue {
       return temp.value;
   }
 }
-*/
 
+
+
+
+////// Binary Search Tree  /// Big O(log n) /////////////////////
+
+class Node{
+  constructor(value){
+    this.value=value;
+    this.left=null;
+    this.right=null;
+  }
+}
+
+class BinarySearchTree{
+  constructor(){
+    this.root=null;
+  }
+  insert(value){
+    let newNode=new Node(value);
+    if(this.root===null){
+      this.root=newNode;
+      return this;
+    }else{
+      let current=this.root;
+      while(true){
+        if(value===current.value) return undefined;
+        if(value<current.value){
+          if(current.left===null){
+            current.left=newNode;
+            return this;
+          } else {
+            current=current.left;
+          }
+           
+        }else if(value>current.value){
+          if(current.right===null){
+            current.right=newNode;
+            return this;
+          } else{
+            current=current.right;
+          }
+
+        }
+      }
+    }
+  }
+  find(value){
+    if(this.root===null) return false;
+    let current=this.root,found=false;
+    while(current && !found){
+      if(value<current.value){
+        current=current.left;
+
+      }
+      else if(value>current.value){
+         current=current.right;
+      }else{
+        found=true;
+        break;
+      }
+    }
+    if(!found) return undefined;
+   return current;
+
+   
+  }
+  contains(value){
+    if(this.root===null) return false;
+    let current=this.root,found=false;
+    while(current && !found){
+      if(value<current.value){
+        current=current.left;
+
+      }
+      else if(value>current.value){
+         current=current.right;
+      }else{
+       return true;
+      }
+    }
+   return false;
+
+   
+  }
+}
+const tree=new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
+console.log(tree.contains(4));
+// tree.root=new Node(10);
+// tree.root.right= new Node(10);
+// tree.root.left=new Node(7);
+// tree.root.left.right=new Node(9);
+
+
+
+////////////////// BFS DFS (Breadth First Search and Depth First Search)///////////////////
+
+class Node{
+  constructor(value){
+    this.value=value;
+    this.left=null;
+    this.right=null;
+  }
+}
+
+class BinarySearchTree{
+  constructor(){
+    this.root=null;
+  }
+  insert(value){
+    let newNode=new Node(value);
+    if(this.root===null){
+      this.root=newNode;
+      return this;
+    }else{
+      let current=this.root;
+      while(true){
+        if(value===current.value) return undefined;
+        if(value<current.value){
+          if(current.left===null){
+            current.left=newNode;
+            return this;
+          } else {
+            current=current.left;
+          }
+           
+        }else if(value>current.value){
+          if(current.right===null){
+            current.right=newNode;
+            return this;
+          } else{
+            current=current.right;
+          }
+
+        }
+      }
+    }
+  }
+  find(value){
+    if(this.root===null) return false;
+    let current=this.root,found=false;
+    while(current && !found){
+      if(value<current.value){
+        current=current.left;
+
+      }
+      else if(value>current.value){
+         current=current.right;
+      }else{
+        found=true;
+        break;
+      }
+    }
+    if(!found) return undefined;
+   return current;
+
+   
+  }
+  contains(value){
+    if(this.root===null) return false;
+    let current=this.root,found=false;
+    while(current && !found){
+      if(value<current.value){
+        current=current.left;
+
+      }
+      else if(value>current.value){
+         current=current.right;
+      }else{
+       return true;
+      }
+    }
+   return false;   
+  }
+  BFS(){
+    var node=this.root,data=[],queue=[];
+    queue.push(node);
+    while(queue.length){
+      node=queue.shift();
+      data.push(node.value);
+      if(node.left) queue.push(node.left);
+      if(node.right) queue.push(node.right);
+
+    }
+    return data;
+  }
+  DFSPreOrder(){
+    let data=[];  
+    let current=this.root;
+    function traverse(node){
+      data.push(node.value);
+      if(node.left){
+        traverse(node.left)
+      }
+      if(node.right) traverse(node.right);
+    }
+    traverse(current);
+   return data;
+  }
+  DFSPostOrder(){
+    let data=[];
+    let current=this.root;
+    function traverse(node){
+      if(node.left) traverse(node.left);
+      if(node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(current);
+    return data;
+  }
+  DFSInOrder(){
+    let data=[];
+    let current=this.root;
+    function traverse(node){
+      if(node.left) traverse(node.left);       
+        
+        data.push(node.value);
+     if(node.right) traverse(node.right);
+    
+    }
+    traverse(current);
+    return data;
+  }
+}
+const tree=new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
+
+
+*/
 
